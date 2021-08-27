f6697ed0be7a2e0ba6ca6c2e37c4fd914dc0b230
goatryder <nova9ok@gmail.com>
2021-08-27 03:00:33 +0300

    - calc leg ik alpha values and hip bone offset value wip

--- Source/SwordFight/Private/Player/SFCharacterAnim.cpp
+++ Source/SwordFight/Private/Player/SFCharacterAnim.cpp
@@ -9,15 +9,24 @@
 USFCharacterAnim::USFCharacterAnim()
 {
 	// defaults
-	IKFootR = FName("Foot_R");
-	IKFootL = FName("Foot_L");
+	IKFootRBoneName = FName("Foot_R");
+	IKFootLBoneName = FName("Foot_L");
+	HipBoneName = FName("Pelvis");
+	RootBoneName= FName("Root");
 
 	FootBoneZOffset = 13.5f;
+	MaxFootIKTraceDist = 100.f;
+
+	FootIKTraceChannel = ECollisionChannel::ECC_Camera;
+
+	IKFootTraceQueryParams.bTraceComplex = true;
 }
 
 void USFCharacterAnim::NativeInitializeAnimation()
 {
 	SFCharacter = Cast<ASFCharacter>(TryGetPawnOwner());
+
+	IKFootTraceQueryParams.AddIgnoredActor(SFCharacter);
 }
 
 void USFCharacterAnim::NativeUpdateAnimation(float DeltaSeconds)
@@ -29,34 +38,82 @@ void USFCharacterAnim::NativeUpdateAnimation(float DeltaSeconds)
 	}
 
 	CalcMeshBottomFootPositions();
+	CalcLegIKAlphaValues();
 
 	DrawDebugFootIK();
 }
 
 FORCEINLINE void USFCharacterAnim::CalcMeshBottomFootPositions()
 {
-	const FVector BoneFootLocation_R = SFCharacter->GetMesh()->GetSocketLocation(IKFootR);
-	const FVector BoneFootLocation_L = SFCharacter->GetMesh()->GetSocketLocation(IKFootL);
+	const FVector BoneFootLocation_R = SFCharacter->GetMesh()->GetSocketLocation(IKFootRBoneName);
+	const FVector BoneFootLocation_L = SFCharacter->GetMesh()->GetSocketLocation(IKFootLBoneName);
 	
-	const float BaseOffsetZ = FootBoneZOffset * SFCharacter->GetMesh()->GetComponentScale().Z;
-	const FVector FootBottomOffset_R = SFCharacter->GetMesh()->GetSocketQuaternion(IKFootR).GetForwardVector() * BaseOffsetZ;
-	const FVector FootBottomOffset_L = SFCharacter->GetMesh()->GetSocketQuaternion(IKFootL).GetForwardVector() * BaseOffsetZ;
+	const FVector BaseOffsetZVec = SFCharacter->GetMesh()->GetComponentScale() * FootBoneZOffset;
+	const FVector FootBottomOffset_R = SFCharacter->GetMesh()->GetSocketQuaternion(IKFootRBoneName).GetForwardVector() * BaseOffsetZVec;
+	const FVector FootBottomOffset_L = SFCharacter->GetMesh()->GetSocketQuaternion(IKFootLBoneName).GetForwardVector() * BaseOffsetZVec;
 
 	MeshBottomFootR = BoneFootLocation_R + FootBottomOffset_R;  // wtf???
 	MeshBottomFootL = BoneFootLocation_L - FootBottomOffset_L;
+}
 
-	//DrawDebugSphere(GetWorld(), MeshBottomFootL, 6, 36, FColor::Red);
-	//DrawDebugSphere(GetWorld(), MeshBottomFootR, 6, 36, FColor::Red);
+FORCEINLINE void USFCharacterAnim::LineTraceFoot(const FVector& InInitialFootPosition, FHitResult& InFootTraceResult)
+{
+	const FVector IKFoot_Start = FVector(InInitialFootPosition.X, InInitialFootPosition.Y, InInitialFootPosition.Z + MaxFootIKTraceDist);
+	const FVector IKFoot_End = FVector(InInitialFootPosition.X, InInitialFootPosition.Y, InInitialFootPosition.Z - MaxFootIKTraceDist);
+
+	GetWorld()->LineTraceSingleByChannel(InFootTraceResult, IKFoot_Start, IKFoot_End, FootIKTraceChannel, IKFootTraceQueryParams);
+}
+
+FORCEINLINE void USFCharacterAnim::CalcLegIKAlphaValues()
+{
+	// todo fix
+
+	const float MagicAlphaValue = 20.f; // todo: remove magic
+
+	const FVector RootBoneLocation = SFCharacter->GetMesh()->GetSocketLocation(RootBoneName);
+
+	// line trace Right foot ik position
+	FHitResult IKFootTraceResult_R;
+	const FVector IKFootInitialPosition_R = FVector(MeshBottomFootR.X, MeshBottomFootR.Y, MeshBottomFootR.Z + RootBoneLocation.Z);
+	 
+	if (IKFootTraceResult_R.bBlockingHit)
+	{
+		IKAlphaLegRight = (IKFootTraceResult_R.Location - MeshBottomFootR).Z / MagicAlphaValue;
+	}
+	else
+	{
+		IKAlphaLegRight = 0.f;
+	}
+
+	// line trace Left foot ik position
+	FHitResult IKFootTraceResult_L;
+	const FVector IKFootInitialPosition_L = FVector(MeshBottomFootL.X, MeshBottomFootL.Y, MeshBottomFootL.Z + RootBoneLocation.Z);
+
+	if (IKFootTraceResult_L.bBlockingHit)
+	{
+		IKAlphaLegLeft = (IKFootTraceResult_L.Location - MeshBottomFootL).Z / MagicAlphaValue;
+	}
+	else
+	{
+		IKAlphaLegLeft = 0.f;
+	}
+
+	// hip displacement
+	IKLegHipDisplacementZ = (IKFootTraceResult_R.Location - IKFootTraceResult_L.Location).Z * -0.5f;
 }
 
 FORCEINLINE void USFCharacterAnim::DrawDebugFootIK() const
 {
-	const FVector IkFootL_Normal = SFCharacter->GetMesh()->GetSocketQuaternion(IKFootL).GetForwardVector();
-	const FVector IkFootR_Normal = SFCharacter->GetMesh()->GetSocketQuaternion(IKFootR).GetForwardVector();
+	const FVector IkFootL_Normal = SFCharacter->GetMesh()->GetSocketQuaternion(IKFootLBoneName).GetForwardVector();
+	const FVector IkFootR_Normal = SFCharacter->GetMesh()->GetSocketQuaternion(IKFootRBoneName).GetForwardVector();
 
 	FPlane PlaneL = FPlane(MeshBottomFootL, IkFootL_Normal);
 	FPlane PlaneR = FPlane(MeshBottomFootR, IkFootR_Normal);
 
 	DrawDebugSolidPlane(GetWorld(), PlaneL, MeshBottomFootL, 15, FColor::Cyan);
 	DrawDebugSolidPlane(GetWorld(), PlaneR, MeshBottomFootR, 15, FColor::Cyan);
+
+	DrawDebugString(GetWorld(), MeshBottomFootL, FString("IKLegAlpha: ") + FString::SanitizeFloat(IKAlphaLegLeft), 0, FColor::White, 0.f, true);
+	DrawDebugString(GetWorld(), MeshBottomFootR, FString("IKLegAlpha: ") + FString::SanitizeFloat(IKAlphaLegRight), 0, FColor::White, 0.f, true);
+	DrawDebugString(GetWorld(), SFCharacter->GetMesh()->GetSocketLocation(HipBoneName), FString("HipDisplacement: ") + FString::SanitizeFloat(IKLegHipDisplacementZ), 0, FColor::White, 0.f, true);
 }
--- Source/SwordFight/Public/Player/SFCharacterAnim.h
+++ Source/SwordFight/Public/Player/SFCharacterAnim.h
@@ -16,40 +16,77 @@ class SWORDFIGHT_API USFCharacterAnim : public UAnimInstance
 
 private:
 	FORCEINLINE void DrawDebugFootIK() const;
-
-	FVector MeshBottomFootR;
-	FVector MeshBottomFootL;
 	
 	// calculate right offset in 3d space for foot mesh bottom using FootBoneZOffset
 	FORCEINLINE void CalcMeshBottomFootPositions();
+	
+	/** Handle line trace for foot
+	
+	* @param InInitialFootPosition : where foot is should be located if IK disabled. should be vector: bone location xy and root z
+	* @param InFootTraceResult : foot ik trace result out
+	*/
+	FORCEINLINE void LineTraceFoot(const FVector& InInitialFootPosition, FHitResult& InFootTraceResult);
+
+	// calculates IKAlphaLegLeft, IKAlphaLegRight, IKLegHipDisplacement
+	FORCEINLINE void CalcLegIKAlphaValues();
+
+	// default IK Foot trace query params
+	FCollisionQueryParams IKFootTraceQueryParams;
 
 public:
 	USFCharacterAnim();
-	
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-	class ASFCharacter* SFCharacter;
 
 	UPROPERTY(EditDefaultsOnly)
-	FName IKFootR;
+	FName IKFootRBoneName;
 
 	UPROPERTY(EditDefaultsOnly)
-	FName IKFootL;
+	FName IKFootLBoneName;
+
+	UPROPERTY(EditDefaultsOnly)
+	FName HipBoneName;
+
+	UPROPERTY(EditDefaultsOnly)
+	FName RootBoneName;
 	
 	// z distance between foot bone and foot mesh bottom part (applied to default scale size, actual value is change depended on scale)
 	UPROPERTY(EditDefaultsOnly)
 	float FootBoneZOffset;
 
-	// get right foot mesh bottom part position (can handle mesh scale);
-	UFUNCTION(BlueprintCallable)
-	FORCEINLINE FVector GetMeshBottomFootR() const { return MeshBottomFootR; }
+	// max z distance up and down from root to trace foot ik
+	UPROPERTY(EditDefaultsOnly)
+	float MaxFootIKTraceDist;
 
-	// get left foot mesh bottom part position (can handle mesh scale);
-	UFUNCTION(BlueprintCallable)
-	FORCEINLINE FVector GetMeshBottomFootL() const { return MeshBottomFootL; }
+	//UPROPERTY(EditDefaultsOnly)
+	ECollisionChannel FootIKTraceChannel;
 
-	virtual void NativeUpdateAnimation(float DeltaSeconds);
 
-	virtual void NativeInitializeAnimation();
+	UPROPERTY(BlueprintReadOnly)
+	class ASFCharacter* SFCharacter;
+
+	// IK alpha value for right foot LegIK
+	UPROPERTY(BlueprintReadOnly)
+	float IKAlphaLegRight;
+
+	// IK alpha value for left foot LegIK
+	UPROPERTY(BlueprintReadOnly)
+	float IKAlphaLegLeft;
+
+	// Hip bone z displacement during LegIK to allow one leg be relatively lower than root bone
+	UPROPERTY(BlueprintReadOnly)
+	float IKLegHipDisplacementZ;
+
+	// right foot mesh bottom part position (stable to mesh scale);
+	UPROPERTY(BlueprintReadOnly)
+	FVector MeshBottomFootR;
+
+	// left foot mesh bottom part position (stable to mesh scale);
+	UPROPERTY(BlueprintReadOnly)
+	FVector MeshBottomFootL;
+
+
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	virtual void NativeInitializeAnimation() override;
 	
 
 };
